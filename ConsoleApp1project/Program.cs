e3ca651f063800876eb2d19b6114098595231c32
OsamaMosa174 <osamamosa624@gmail.com>
2024-12-18 17:16:52 +0200

    learn methouds

--- ConsoleApp1project/Program.cs
+++ ConsoleApp1project/Program.cs
@@ -2,6 +2,10 @@
 
 class Program
 {
+    static int addnumbers(int number1, int number2)
+    {
+        return number1 + number2;
+    }
     static void Main(string[] args)
     {
         int[] number = new int[3];
@@ -13,16 +17,26 @@ class Program
             Console.WriteLine(numbers);
         }
 
-        List<object>num = new List<object>();
+        List<object> num = new List<object>();
         num.Add(10);
         num.Add(20);
         num.Add(30);
         num.Add("osama");
-     
+
         foreach (object allnum in num)
         {
             Console.WriteLine(allnum);
-       
+
         }
+
+
+        Console.Write("Enter frist number: ");
+        int fristnumber= Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter frist second: ");
+        int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+        int result=addnumbers(fristnumber, secondnumber);
+        Console.WriteLine($"\nThe sum of {fristnumber} and {secondnumber} is {result}.");
+
     }
 }
--- ConsoleApp1project/obj/Debug/net8.0/ConsoleApp1project.AssemblyInfo.cs
+++ ConsoleApp1project/obj/Debug/net8.0/ConsoleApp1project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+166ba6f9105db2839f0cef17483c7ab6e0287077")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d075c75baa37e2e2dc3af55f874a25cb525289a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
